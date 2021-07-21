6cd46ce1ca31ab000c13f39e7c61f2b3e2bf137a
Debodyuti Biswas <59243952+debodyuti1@users.noreply.github.com>
2021-07-21 19:53:24 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,12 @@
+var RandomNumber1 = Math.floor(Math.random() * 6) + 1;  // 1-6
+var RandomDiceImage1 = "dice" + RandomNumber1 + ".png";  // dice1.png - dice6.png
+var RandomImageSource1 = "images/" + RandomDiceImage1;  // images/dice1.png - images/dice6.png
+var image1 = document.querySelectorAll("img")[0];
+image1.setAttribute("src", RandomImageSource1);
+
+var RandomNumber2 = Math.floor(Math.random() * 6) + 1;
+var RandomDiceImage2 = "dice" + RandomNumber2 + ".png";
+var RandomImageSource2 = "images/" + RandomDiceImage2;
+var image2 = document.querySelectorAll("img")[1];
+image2.setAttribute("src", RandomImageSource2);
+
