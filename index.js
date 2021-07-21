544fd90955fa6256c2bb1511cc8dc8b1bfc84c75
debodyuti1 <db8145@srmist.edu.in>
2021-07-21 21:06:12 +0530

    heading changes

--- index.js
+++ index.js
@@ -12,3 +12,12 @@ var RandomImageSource2 = "images/" + RandomDiceImage2;
 var image2 = document.querySelectorAll("img")[1];
 image2.setAttribute("src", RandomImageSource2);
 
+if (RandomNumber1>RandomNumber2) {
+    document.querySelector("h1").innerHTML = "Player 1 Wins!";
+}
+else if (RandomNumber2>RandomNumber1) {
+    document.querySelector("h1").innerHTML = "Player 2 Wins!";
+}
+else {
+    document.querySelector("h1").innerHTML = "It's a Draw :(";
+}
